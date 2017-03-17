0a239a8a4ac08512fef95d16d6b5b110efdc17ba
Megan Warnock <megandwarnock@gmail.com>
2017-03-17 09:30:35 -0700

    pass second test successfully.

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,10 +1,11 @@
 public class Event {
+  private int mPeople;
 
   public Event(int people, String food, String beverage, String enterainment) {
-
+    mPeople = people;
   }
 
   public int getsNumberOfPeople() {
-    return 0;
+    return mPeople;
   }
 }
