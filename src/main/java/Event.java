219dc850f8489eae4867efb70d6f40a5e26851c7
Megan Warnock <megandwarnock@gmail.com>
2017-03-17 09:23:19 -0700

    successfully fail second test.

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,6 +1,10 @@
 public class Event {
 
   public Event(int people, String food, String beverage, String enterainment) {
-    
+
+  }
+
+  public int getsNumberOfPeople() {
+    return 0;
   }
 }
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -5,8 +5,14 @@ public class EventTest {
 
   @Test
   public void newEvent_instantiatesCorrectly() {
-  Event testEvent = new Event(1, "entree", "beer", "music");
-  assertEquals(true, testEvent instanceof Event);
+    Event testEvent = new Event(1, "entree", "beer", "music");
+    assertEquals(true, testEvent instanceof Event);
+  }
+
+  @Test
+  public void newEvent_getsNumberOfPeople_1() {
+    Event testEvent = new Event(1, "entree", "beer", "music");
+    assertEquals(1, testEvent.getsNumberOfPeople());
   }
 
 }
